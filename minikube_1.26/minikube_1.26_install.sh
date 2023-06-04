7d5a7535acec11d69216bcad00c12bbd943b0767
PrasannaRam <49111672+prasannaram19591@users.noreply.github.com>
2023-06-04 21:15:05 +0530

    Update minikube_1.26_install.sh

--- minikube_1.26/minikube_1.26_install.sh
+++ minikube_1.26/minikube_1.26_install.sh
@@ -11,15 +11,14 @@ systemctl enable --now docker
 dnf install conntrack -y
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+mv ./kubectl /usr/local/bin/kubectl
 cp /usr/local/bin/kubectl /usr/local/sbin/kubectl
 kubectl version
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube
 mkdir -p /usr/local/bin/
 install minikube /usr/local/bin/
-cp minikube /usr/local/sbin
-rm -f minikube 
+mv minikube /usr/local/sbin
 minikube version
 yum install -y git
 git clone https://github.com/Mirantis/cri-dockerd.git
@@ -37,19 +36,21 @@ cp /usr/local/bin/cri-dockerd /usr/local/sbin/cri-dockerd
 systemctl daemon-reload
 systemctl enable --now cri-docker.socket
 systemctl is-active cri-docker.socket
+cd
 git clone https://github.com/prasannaram19591/source-code.git
-cd /root/source-code/k8s_crio_repo/
+cd /root/source-code/minikube_1.26/
 cp *.repo /etc/yum.repos.d/
 yum install cri-o cri-tools -y
 systemctl enable --now crio
 systemctl is-enabled crio
 cd
-cp /usr/local/bin/cri-dockerd .
+\cp /usr/local/bin/cri-dockerd .
 minikube start --driver=none
 minikube status
 kubectl cluster-info
 kubectl get nodes
 systemctl enable --now kubelet cri-docker
 systemctl is-active docker crio kubelet cri-docker
+systemctl is-enabled docker crio kubelet cri-docker
 kubectl taint nodes $(hostname) node.kubernetes.io/not-ready-
 kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
